f195db53386ce987644be6f892303961b12a2a75
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-10-07 11:50:48 +0900

    [feat] GNB에 로고 아이콘  추가

--- components/GNB.tsx
+++ components/GNB.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
 
 const GNB = () => {
   return (
     <header className="w-full max-w-screen-2xl  lg:px-[120px] mx-auto h-[70px] flex justify-between items-center px-8 bg-black text-white shadow-md fixed top-0 z-10">
-      {/* Logo */}
-      <div className="text-title-1 font-bold cursor-pointer">
-        CoinScope
-      </div>
+     {/* 로고 영역 */}
+  <div className="flex items-center">
+    {/* PNG 아이콘 추가 (next/image) */}
+    <Image 
+      src="/images/logo_icon.png" 
+      alt="Logo Icon" 
+      width={16.538} 
+      height={19.636} 
+      className="flex-shrink-0"
+    />
 
+    {/* CoinScope 텍스트 */}
+    <div className="ml-2 text-title-1 font-bold cursor-pointer">
+      CoinScope
+    </div>
+  </div>
       {/* Navigation Links */}
       <nav className="flex space-x-8 ">
         <div className="px-5 py-1.5 rounded-[20px] flex items-center">
@@ -51,10 +63,52 @@ const GNB = () => {
 
 
         {/* Language Selection */}
-        <div className="pl-2 py-[7.30px] rounded-[65.11px] flex items-center">
-          <div className="text-[#a6a8b0] text-sm font-medium font-['SUIT'] leading-relaxed">EN</div>
+        <div className="flex items-center gap-1 gap-x-2">
+  {/* 아이콘 */}
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+  >
+    <g clipPath="url(#clip0_3011_3125)">
+      <path
+        d="M7.00002 12.8332C10.2217 12.8332 12.8334 10.2215 12.8334 6.99984C12.8334 3.77818 10.2217 1.1665 7.00002 1.1665C3.77836 1.1665 1.16669 3.77818 1.16669 6.99984C1.16669 10.2215 3.77836 12.8332 7.00002 12.8332Z"
+        stroke="#A6A8B0"
+        strokeWidth="1.16667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.16669 7H12.8334"
+        stroke="#A6A8B0"
+        strokeWidth="1.16667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.00002 1.1665C8.4591 2.76388 9.28829 4.83686 9.33335 6.99984C9.28829 9.16282 8.4591 11.2358 7.00002 12.8332C5.54094 11.2358 4.71175 9.16282 4.66669 6.99984C4.71175 4.83686 5.54094 2.76388 7.00002 1.1665Z"
+        stroke="#A6A8B0"
+        strokeWidth="1.16667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_3011_3125">
+        <rect width="14" height="14" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+
+  {/* EN 글씨 */}
+  <div className="text-[#a6a8b0] pt-1 text-sm font-medium font-['SUIT'] leading-relaxed">
+    EN
+  </div>
+</div>
         </div>
-      </div>
+
     </header>
   );
 };
